e1aaa6fb2083dc12de665d10e8ca04de2f83e1c3
Pierre Emmanuel JOUBERT <pierre@redkaffe.com>
2017-11-29 17:33:48 +0100

    Protect OUs in Domain against deletion
    Script to easily protect all OUs in domain against accidental Deletion.
    Can be used also to Unprotect All OUs :)

---
+++ ProtectOUs.ps1
@@ -0,0 +1,5 @@
+﻿$searchbase = "DC=CORP,DC=REDKAFFE,DC=COM" 
+         
+#Protect Organizational units against accidental deletion 
+import-module activedirectory 
+Get-ADOrganizationalUnit -searchbase $searchbase -filter * -Properties ProtectedFromAccidentalDeletion | where {$_.ProtectedFromAccidentalDeletion -eq $false} | Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $true
